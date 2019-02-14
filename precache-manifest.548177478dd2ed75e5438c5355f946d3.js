daf6449452963958f2becf58ceb19e82e38ce891
Shiau Kang Wang <skw321@Shiaus-MacBook-Pro.local>
2019-02-14 02:19:53 -0800

    Updates

---
+++ precache-manifest.548177478dd2ed75e5438c5355f946d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "362cc621a0392d9ea13b",
+    "url": "/Mock-Netflix-Page/static/css/main.34f6a972.chunk.css"
+  },
+  {
+    "revision": "362cc621a0392d9ea13b",
+    "url": "/Mock-Netflix-Page/static/js/main.362cc621.chunk.js"
+  },
+  {
+    "revision": "8275a475325495b200f5",
+    "url": "/Mock-Netflix-Page/static/js/runtime~main.8275a475.js"
+  },
+  {
+    "revision": "908d0aa7cca9a1c8aeaf",
+    "url": "/Mock-Netflix-Page/static/js/2.908d0aa7.chunk.js"
+  },
+  {
+    "revision": "140b11729b0c3188706d33a635d19656",
+    "url": "/Mock-Netflix-Page/index.html"
+  }
+];
